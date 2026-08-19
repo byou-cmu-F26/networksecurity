f30340e3e364d4a94722ab7a65fc0bd10a39b251
bihao <bihao@bihaodeMacBook-Air.local>
2026-08-19 21:51:23 +0800

    Setup.py file ready

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirments()->List[str]:
+
+    requirement_lst:List[str]=[]
+
+    try:
+        with open('requirements.txt','r') as file:
+            lines = file.readlines()
+            for line in lines:
+                requirement=line.strip()
+                if requirement and requirement !='-e .':
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt not found ")
+
+    return requirement_lst
+
+# print(get_requirments())
+
+setup(
+    name="Network Security",
+    version="0.0.1",
+    author="bihao you",
+    author_email="bihao.you22@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirments()
+)
